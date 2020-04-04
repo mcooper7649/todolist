be60e8a8f1619611feccffb96a2f3d86eb91c165
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-04-03 20:48:03 -0400

    working single list

--- app.js
+++ app.js
@@ -70,6 +70,18 @@ app.post("/", function(req, res){
 
 });
 
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox; 
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (err){
+      console.log("Error")
+    }else{
+      console.log("Successfully Deleted Item")
+      res.redirect("/")
+    }
+  });
+})
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
